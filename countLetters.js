14c01ed410a3f93eacb4ab076a421ae79e555332
Filipa Marta <filipa.marta@gmail.com>
2020-05-05 11:34:20 +0100

    group4

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,24 @@
 const countLetters = (str) => {
-    // your code should go here 
-}
+  let result = {};
+  const arr = str.split("");
+  const res = arr.map((el, i) => {
+    console.log(el);
+
+    if (el in result) {
+      result[el] = result[el] + 1;
+    } else {
+      result[el] = 1;
+    }
+  });
+  console.log(result);
+  return result;
+};
+
+countLetters("hello");
 
 module.exports = countLetters;
+
+//create empty results object
+//transform the string in an array by using split
+//iterate in the array string with a map or foreach
+//
