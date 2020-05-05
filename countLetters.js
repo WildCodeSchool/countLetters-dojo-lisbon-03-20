bb80ce7873ca9143376ece0ce46856bc162b5197
RoxyPursey <roxy.pursey@gmail.com>
2020-05-05 11:43:54 +0100

    completed dojo

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,13 @@
 const countLetters = (str) => {
-    // your code should go here 
-}
-
+  const newObject = {};
+  str.split('').forEach((letter) => {
+    newObject[letter] = newObject[letter] ? newObject[letter] + 1 : 1;
+  });
+  return newObject;
+};
+console.log(countLetters('Isadora Haas Hillebrand'));
 module.exports = countLetters;
+
+//count the frequency of each letter inside the string
+// use split to devide each string into letters
+// use forEach method to loop through string
