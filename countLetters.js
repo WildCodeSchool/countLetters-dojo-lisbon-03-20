aeb6fc965d2cb0d4b3f6de9c8d21f33d8a6f3fae
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-05-05 10:58:22 +0100

    count done

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,29 @@
 const countLetters = (str) => {
-    // your code should go here 
+    let result = {}
+    const arr = str.split('')
+    let newResult = arr.map((letter) => {
+        if( result[letter] === undefined ){
+            result[letter] = 1;
+        } else {
+            result[letter]= result[letter] + 1
+        }
+    })
+    return result;
 }
 
 module.exports = countLetters;
+
+//create a result object
+//Iterate trough the str (for loop or map)
+// create an object with the char as key, and the count as value
+//count the number of times a char is present in the str
+
+//console.log(countLetters('hello'))
+
+// returns:
+// {
+//     'h': 1,
+//     'e': 1,
+//     'l': 2,
+//     'o': 1,
+// }
